8b4cb7fc119f97adf7180618537599d93ddb087d
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-30 08:14:02 -0700

    Change module name, add do while loop, and added variable

---
+++ ProgramFlow/ProgramFlow.vb
@@ -0,0 +1,21 @@
+﻿
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module ProgramFlow
+
+    Sub Main()
+        Dim FirstNumber As Integer
+
+        Do
+            Console.WriteLine("In the second loop. Loop While FirstNumber <= 3")
+            FirstNumber += 1
+            Console.WriteLine($"FirstNUmber is {FirstNumber}!")
+        Loop While FirstNumber <= 3
+
+        Console.WriteLine()
+
+    End Sub
+
+End Module
