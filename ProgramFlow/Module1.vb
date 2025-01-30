5f20c01505c5f4ffc81baf8162e61343b2a9a5c1
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-30 08:06:51 -0700

    Add project files.

---
+++ ProgramFlow/Module1.vb
@@ -0,0 +1,12 @@
+﻿
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ ProgramFlow/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ProgramFlow")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ProgramFlow")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f8c0584a-5d04-416c-a0bc-9af3c16e4568")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
